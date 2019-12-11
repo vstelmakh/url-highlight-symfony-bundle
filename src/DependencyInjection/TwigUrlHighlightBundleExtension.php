a62703c5fa598f1705f4f7c66f524270265d290c
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2019-12-12 00:19:31 +0100

    add extension and service definition

---
+++ src/DependencyInjection/TwigUrlHighlightBundleExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace VStelmakh\Bundle\TwigUrlHighlightBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class TwigUrlHighlightBundleExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
