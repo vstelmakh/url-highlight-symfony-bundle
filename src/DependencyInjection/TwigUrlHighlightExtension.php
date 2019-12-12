a3d93eb605cdc52dbff5ab7ed5bdf60b6ccab19c
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2019-12-13 00:14:20 +0100

    fix symfony extension class name

--- src/DependencyInjection/TwigUrlHighlightExtension.php
+++ src/DependencyInjection/TwigUrlHighlightExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class TwigUrlHighlightBundleExtension extends Extension
+class TwigUrlHighlightExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
